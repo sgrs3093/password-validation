d00c4a94929865542935d472a29c17ad1a331d3e
sagar <sgrs3093@gmail.com>
2021-07-03 11:45:28 +0530

    added flag for validation

--- src/main/java/com/password/service/PasswordVerificationService.java
+++ src/main/java/com/password/service/PasswordVerificationService.java
@@ -1,5 +1,6 @@
 package com.password.service;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 import com.password.exception.NotValidException;
@@ -11,12 +12,17 @@ public class PasswordVerificationService implements IPasswordVerificationService
 	Pattern lowerCasePatten = Pattern.compile("[a-z ]");
 	Pattern digitCasePatten = Pattern.compile("[0-9 ]");
 
+	boolean flag = true;
+	List<String> errorList;
+
 	public void verifyPassword(String password) {
 
 	}
 
 	public void validateLength(String s) throws NotValidException {
 		if (s.length() < 8) {
+			flag = false;
+			errorList.add("length should be grater than 8 characters");
 			throw new NotValidException("length should be grater than 8 characters");
 		}
 
@@ -24,7 +30,8 @@ public class PasswordVerificationService implements IPasswordVerificationService
 
 	public void isEmpty(String pass) throws NotValidException {
 		if (pass.length() == 0 || pass.isEmpty() || pass == null) {
-
+			flag = false;
+			errorList.add("password should not be empty");
 			throw new NotValidException("password should not be empty");
 		}
 
@@ -32,6 +39,8 @@ public class PasswordVerificationService implements IPasswordVerificationService
 
 	public void shouldHaveOneUppercase(String s) throws NotValidException {
 		if (!UpperCasePatten.matcher(s).find()) {
+			flag = false;
+			errorList.add("Password must have atleast one uppercase character !!");
 			throw new NotValidException("Password must have atleast one uppercase character !!");
 		}
 
@@ -39,6 +48,8 @@ public class PasswordVerificationService implements IPasswordVerificationService
 
 	public void shouldHaveOneLowercase(String s) throws NotValidException {
 		if (!lowerCasePatten.matcher(s).find()) {
+			flag = false;
+			errorList.add("Password must have atleast one lowercase character !!");
 			throw new NotValidException("Password must have atleast one lowercase character !!");
 		}
 
@@ -46,6 +57,8 @@ public class PasswordVerificationService implements IPasswordVerificationService
 
 	public void shouldHaveOneNumber(String pass) throws NotValidException {
 		if (!digitCasePatten.matcher(pass).find()) {
+			flag = false;
+			errorList.add("Password must have atleast one digit character !!");
 			throw new NotValidException("Password must have atleast one digit character !!");
 		}
 
